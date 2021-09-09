4757eade2f0275aefabe570ae34315a5c8953f66
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-09 17:01:46 -0300

    Create questao8.py

---
+++ questao8.py
@@ -0,0 +1,13 @@
+#Nicolas Andrade de Freitas
+#Turma 2190
+
+lista_pc = ["Intel I7", "16GB de Ram", "1TB de HD", "240GB SSD", "Fonte 700W", "GeForce RTX2080Ti", "Gabinete Cooler Master"]
+print("Meu PC Gamer" ,lista_pc)
+#crio a lista com as configurações do pc
+lista_pc.remove("GeForce RTX2080Ti")
+print(lista_pc)
+#removo a RTX2080Ti
+lista_pc.remove("Gabinete Cooler Master")
+print(lista_pc)
+#removo o Gabinete Cooler Master
+
