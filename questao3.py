5e8e362363d8db1541a99e2292122931902959a7
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-09 15:48:36 -0300

    Create questao3.py
    Criação da questão 3

---
+++ questao3.py
@@ -0,0 +1,16 @@
+#Nicolas Andrade de Freitas
+#Turma 2190
+
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+n3 = int(input("Digite o terceiro numero: "))
+#Peço tres numeros para fazer a soma
+
+lista = [n1, n2, n3]
+#coloco os numeros na lista
+
+soma = sum(lista)
+#faço a soma
+
+print("A soma dos Numeros anteriores é de: {}".format(soma))
+#mostro a soma 
