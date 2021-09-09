b388124cfc19312af3a88e5f81a02ca115a23f25
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-09 17:05:29 -0300

    Create questao9.py

---
+++ questao9.py
@@ -0,0 +1,8 @@
+#Nicolas Andrade de Freitas
+#Turma 2190
+
+lista_numeros = [10,4,5,6,232,9,77,7]
+#lista com numeros de forma aleatoria 
+
+print("A lista de forma organizada: ", sorted(lista_numeros))
+#mostro a lista de forma organizada
